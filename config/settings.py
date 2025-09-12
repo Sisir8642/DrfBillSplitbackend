345ca427bad77342c86770b4d77d54a49fe42ffe
Sisir8642 <baburambista01@gmail.com>
2025-09-13 01:14:21 +0545

    worked on the CORS policy solving

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,8 +158,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -173,10 +177,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    
-    
+    "http://localhost:3000",  # local dev
+    "https://next-bill-spit-frontend.vercel.app",  # production domain
+    "https://next-bill-spit-frontend-git-main-baburams-projects.vercel.app",  # main branch preview
 ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
