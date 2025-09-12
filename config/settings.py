6bd8d1b138110c4b1ecb69b0324c8f84b9aa366f
Sisir8642 <baburambista01@gmail.com>
2025-09-12 20:46:22 +0545

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://bro-splitter.onrender.com"]
+ALLOWED_HOSTS = ['bro-splitter.onrender.com', 'localhost', '127.0.0.1']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
